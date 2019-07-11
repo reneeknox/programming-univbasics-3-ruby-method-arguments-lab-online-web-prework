4679c5ed130dc46eb4a2a1718d8cb48be83be234
Renee France <renee5.0@outlook.com>
2019-07-11 21:57:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,16 +1,16 @@
    #method name    #parameter
-def introduction(name)
+def #introduction(name)
 p "Hi, my name is #{name}."
 end
 
 
   #method name    first_parameter, second_parameter
-def introduction_with_language(name, language)
+def #introduction_with_language(name, language)
 p "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 #method name    first_parameter, second_parameter
-def introduction_with_language_optional(name, language)
+def #introduction_with_language_optional(name, language)
 p "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
